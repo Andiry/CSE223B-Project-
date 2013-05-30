05e7837e1ed1ff9f64176595a3b76cd1bb8c766a
Andiry Xu <jix024@cs.ucsd.edu>
2013-05-30 16:05:04 -0700

    Add '-f' option to FUSE

--- joeExperiment/fuse_operations.cpp
+++ joeExperiment/fuse_operations.cpp
@@ -233,8 +233,9 @@ static int fuseMain(int argc, char *argv[],
 void * startFuse(void * arg) {
     cerr << "Starting FUSE..." << endl;
     ArgStruct * args = (ArgStruct *) arg;
-    int argc = 2;
+    int argc = 3;
     backup_path = args->argv[2];
+    args->argv[2] = "-f";
 
     fuse_operations from_fuse_oper;
     initOpers(from_fuse_oper);
